67e3ca67c21685db45b523f50bf4c35a1f3d5e13
dailar <dailarse00@gmail.com>
2023-02-10 12:36:21 +0300

    done

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,26 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> result = new ArrayDeque<>();
+        while (!(firstQueue.isEmpty() && secondQueue.isEmpty())) {
+            Integer resultCard = result.poll();
+            if (resultCard != null) {
+                firstQueue.offer(resultCard);
+            }
+            resultCard = result.poll();
+            if (resultCard != null) {
+                secondQueue.offer(resultCard);
+            }
+
+            for (int i = 0; i < 2; i++) {
+                if (!firstQueue.isEmpty())
+                    result.offer(firstQueue.poll());
+            }
+            for (int i = 0; i < 2; i++) {
+                if (!secondQueue.isEmpty())
+                    result.offer(secondQueue.poll());
+            }
+        }
+        return result;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,32 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 1; i < numberOfDishes + 1; i++) {
+            queue.offer(i);
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while (true) {
+            try {
+                for (int i = 1; i < everyDishNumberToEat; i++) {
+                    queue.add(queue.remove());
+                }
+                list.add(queue.remove());
+            } catch (NoSuchElementException e) {
+                break;
+            }
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        DishOrderDeterminer dishOrderDeterminer = new DishOrderDeterminer();
+        dishOrderDeterminer.determineDishOrder(11, 4);
     }
 }
